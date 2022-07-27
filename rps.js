1504b3cca2712b93d9d28bd28be0312b4353f2d7
Jackie Yu <jackieyu247@gmail.com>
2022-07-27 01:27:57 +0100

    Creates game function to simulate n games requiring user input

--- rps.js
+++ rps.js
@@ -16,17 +16,41 @@ function playRound(playerSelection,computerSelection){
     // In case of tie, output appropriate message.
     switch(computerSelection){
         case 'Rock':
-            if(playerSelection == 'Paper') return "You Win! Paper beats Rock";
-            else if(playerSelection == 'Rock')return 'Tie! You both chose ' + computerSelection;
-            else return 'You Lose! Rock beats ' + playerSelection;
+            if(playerSelection == 'Paper'){
+                console.log("You Win! Paper beats Rock");
+                return 1;
+            } else if(playerSelection == 'Rock'){
+                console.log("Tie! You both chose " + computerSelection);
+                return 0;
+            }
+            else{
+                console.log('You Lose! Rock beats ' + playerSelection);
+                return -1;
+            }
         case 'Paper':
-            if(playerSelection == 'Scissors') return "You Win! Scissors beats Rock!";
-            else if(playerSelection == 'Paper')return 'Tie! You both chose ' + computerSelection;
-            else return 'You Lose! Paper beats ' + playerSelection;
+            if(playerSelection == 'Scissors'){
+                console.log("You Win! Scissors beats Paper");
+                return 1;
+            } else if(playerSelection == 'Paper'){
+                console.log("Tie! You both chose " + computerSelection);
+                return 0;
+            }
+            else{
+                console.log('You Lose! Paper beats ' + playerSelection);
+                return -1;
+            }
         case 'Scissors':
-            if(playerSelection == 'Rock') return "You win! Rock beats Scissors!";
-            else if(playerSelection == 'Scissors')return 'Tie! You both chose ' + computerSelection;
-            else return 'You Lose! Scissors beats ' + playerSelection;
+            if(playerSelection == 'Rock'){
+                console.log("You Win! Rock beats Scissors");
+                return 1;
+            } else if(playerSelection == 'Scissors'){
+                console.log("Tie! You both chose " + computerSelection);
+                return 0;
+            }
+            else{
+                console.log('You Lose! Scissors beats ' + playerSelection);
+                return -1;
+            }
     }
 }
 
@@ -35,12 +59,25 @@ function title(choice){
     return choice.charAt(0).toUpperCase() + choice.substr(1).toLowerCase();
 }
 
-// Get user choice 
-let userInput = prompt('Enter rock paper or scissors:');
-//console.log(title(userInput));
-while(!choices.includes(title(userInput))){
-    userInput = prompt('Enter rock paper or scissors:');
+function game(games){
+    let score = 0;
+    for(let i  = 0, n = games; i < n; ++i){
+        let userInput = prompt('Enter rock paper or scissors:');
+        let result = playRound(title(userInput),getComputerChoice());
+        if(result == 1){
+            score++;
+        }
+    }
+    console.log("Your final score was " + score);
+    return score
 }
-// Output result to console.
-console.log(playRound(title(userInput),getComputerChoice()));
 
+// Get user choice 
+//let userInput = prompt('Enter rock paper or scissors:');
+//console.log(title(userInput));
+//while(!choices.includes(title(userInput))){
+//    userInput = prompt('Enter rock paper or scissors:');
+//}
+// Output result to console.
+//console.log(playRound(title(userInput),getComputerChoice()));
+game(5);
