50b5f25acf91feb6fbcde5e42bdef5f04935bbbe
Jackie Yu <jackieyu247@gmail.com>
2022-07-27 01:09:06 +0100

    CreatesplayRound and title functions, also completes user input section

--- rps.js
+++ rps.js
@@ -2,11 +2,41 @@
 // Generate computer answer randomly
 // Create function that plays a single round of rock paper scissors
 // Outputs result of game. 
+choices = ['Rock','Paper','Scissors'];
 
 function getComputerChoice(){
     // Randomly choose rock paper or scissors
-    const choices = ['Rock','Paper','Scissors'];
     const randomChoice = Math.floor(Math.random()*choices.length);
     return choices[randomChoice];
 }
+
+function playRound(playerSelection,computerSelection){
+    // Compare user input with random computer choice.
+    // Rock beats scissors, scissors beats paper, paper beats rock. 
+    switch(computerSelection){
+        case 'Rock':
+            if(playerSelection == 'Paper') return "You Win! Paper beats Rock";
+            else if(playerSelection == 'Rock')return 'Tie! You both chose ' + computerSelection;
+            else return 'You Lose! Rock beats ' + playerSelection;
+        case 'Paper':
+            if(playerSelection == 'Scissors') return "You Win! Scissors beats Rock!";
+            else if(playerSelection == 'Paper')return 'Tie! You both chose ' + computerSelection;
+            else return 'You Lose! Paper beats ' + playerSelection;
+        case 'Scissors':
+            if(playerSelection == 'Rock') return "You win! Rock beats Scissors!";
+            else if(playerSelection == 'Scissors')return 'Tie! You both chose ' + computerSelection;
+            else return 'You Lose! Scissors beats ' + playerSelection;
+    }
+}
+
+function title(choice){
+    return choice.charAt(0).toUpperCase() + choice.substr(1).toLowerCase();
+}
+
+let userInput = prompt('Enter rock paper or scissors:');
+//console.log(title(userInput));
+while(!choices.includes(title(userInput))){
+    userInput = prompt('Enter rock paper or scissors:');
+}
+console.log(playRound(title(userInput),getComputerChoice()));
 
